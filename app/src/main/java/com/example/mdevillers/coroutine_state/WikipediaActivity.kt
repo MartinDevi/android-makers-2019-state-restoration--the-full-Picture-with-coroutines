5ef13d7c16fda62e719e5573698df5e0d100a85d
Martin Devillers <martin.devillers@dashlane.com>
2019-04-17 16:55:07 +0200

    Implement click action with `launch` coroutine

--- app/src/main/java/com/example/mdevillers/coroutine_state/WikipediaActivity.kt
+++ app/src/main/java/com/example/mdevillers/coroutine_state/WikipediaActivity.kt
@@ -2,16 +2,38 @@ package com.example.mdevillers.coroutine_state
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mdevillers.coroutine_state.model.Wikipedia
 import com.example.mdevillers.coroutine_state.view.WikipediaView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
-class WikipediaActivity : AppCompatActivity() {
+class WikipediaActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val view = WikipediaView(this)
 
-        // TODO
+        view.onClickDownloadRandomPage {
+            launch {
+                view.state = WikipediaView.State.ArticleProgress
+                val article = try {
+                    Wikipedia.getRandomArticle()
+                } catch (e: Exception) {
+                    view.state = WikipediaView.State.ArticleError(e)
+                    return@launch
+                }
+                view.state = WikipediaView.State.ArticleDownloaded(article)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancel()
     }
 }
 
